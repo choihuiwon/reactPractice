4cfb1b6fea255a0d2f78235435f1755916a8dd55
최희원 사원 <huiwon.choi@seculayer.com>
2023-12-27 16:33:24 +0900

    React에서 API 호출하기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import React, { useState, useRef } from "react";
-import LifeCycle from "./LifeCycle";
+import React, { useState, useRef, useEffect } from "react";
 
-function App() {
+// https://jsonplaceholder.typicode.com/comments
+
+const App = () => {
   const [data, setData] = useState([]);
 
   const dataId = useRef(0);
+
+  const getData = async () => {
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
@@ -38,11 +60,10 @@ function App() {
 
   return (
     <div className="App">
-      <LifeCycle />
       <DiaryEditor onCreate={onCreate} />
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
   );
-}
+};
 
 export default App;
